84497c67a423bf63e7d013d281c64c1da49f379c
Blake Roddy <you@example.com>
2016-09-09 16:26:39 -0500

    added reset to the form

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,12 +1,11 @@
  $(document).ready(function(){
 	
-	var animals = ["Dog", "Cat", "Penguin", "Hedgehog", "Frog", "Armadillo", "Snake"];
-
+	var animals = ["Chupacabra", "Jackalope", "Penguin", "Hedgehog", "Armadillo", "Snake"];
 
 	addButton();
 
 
-
+// creates buttons for each item in array
 	function addButton(){
 
 		$("#buttons").empty();
@@ -15,9 +14,12 @@
 			var button = $("<button class='btn-primary'>");
 			button.text(animals[i]);
 			button.attr("value", animals[i]);
-			$("#buttons").append(button);		
+			$("#buttons").append(button);	
+			$("#form").trigger("reset");	
 		};
 	};
+
+// adds user input into the array
 
 		$("#form").submit(function(event){
 			event.preventDefault();
