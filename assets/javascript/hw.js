fc7fa7713baf652bb8a9b8764bf47112472f503c
Blake Roddy <you@example.com>
2016-09-09 14:20:18 -0500

    initial array creates buttons on page load

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
+ $(document).ready(function(){
+	
 	var animals = ["Dog", "Cat", "Penguin", "Hedgehog", "Frog", "Armadillo", "Snake"];
 
 
+	addButton();
+
+
+
+	function addButton(){
+		for (var i = 0; i < animals.length; i++) {
+			
+			var button = $("<button>");
+			button.text(animals[i]);
+			button.attr("value", animals[i]);
+			$("#buttons").append(button);
+			
+		};
+	};
 
 
 
@@ -9,7 +24,4 @@ $(document).ready(function(){
 
 
 
-
-
-
-});
+ });
