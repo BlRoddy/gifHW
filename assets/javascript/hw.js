7353ca9cd4c6d6d78c2ccbdafcdd0de9251add1e
Blake Roddy <you@example.com>
2016-09-10 15:40:19 -0500

    got images to change, with bug

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -49,9 +49,11 @@
 		 	for (var i = 0; i < results.length; i++) {
 		 		
 		 		var gifDiv = $('<div>');
+
 		 		var still = results[i].images.fixed_height_still.url;
 		 		var move = results[i].images.fixed_height.url;
 		 		var rating = results[i].rating;
+
 		 		var p = $('<p>').text("Rating: " + rating);
 		 		var image = $('<img>');
 
@@ -65,29 +67,28 @@
 		 		$("#gifs").prepend(gifDiv);
 		 	};
 
-		 });
-	
-	 });
-
 	//need on click function to convert still images to animated
 
-	$(document.body).on('click', 'img', function(){
-		console.log("click works");
+			$("img").click(function(){
+				console.log("click works");
 
-	var state = $(this).attr('data-state');
-		console.log(state);
-	
-		  if (state == 'still'){
-                $(this).attr('src', move);
-                $(this).attr('data-state', 'move');
-            }
+				var state = $(this).attr('data-state');
+				
+			
+				  if (state == 'still'){
+		                $(this).attr("src", move);
+		                $(this).attr('data-state', 'move');
+		            }
 
-            else{
-                $(this).attr('src', still);
-                $(this).attr('data-state', 'still');
-            }
+		            else{
+		                $(this).attr('src', still);
+		                $(this).attr('data-state', 'still');
+		            }
 
+			});
+			
 		});
 
+	 });
 
  });
